ab31004f15424b85d98991cff6eaa8e08862b8ea
Tony <tonythegair@gmail.com>
2016-06-21 12:16:09 +0100

    added a under construction notice to bookings

--- src/dsaui/bookings_gnoga_pkg.adb
+++ src/dsaui/bookings_gnoga_pkg.adb
@@ -4,14 +4,20 @@ procedure Build_Bookings_View
      (App : in out App_Access) is
 
    begin
-      null;
+      if not App.Bookings_Record.Title_Label.Valid then
+         App.Bookings_Record.Title_Label.Create
+           (Parent  => App.Form_Array(Bookings_View),
+            Content => "<H1> Under Construction <H1>");
+      end if;
+
    end Build_Bookings_View;
 
 
 procedure Update_Bookings_View
      (App : in out App_Access) is
    begin
-      null;
+      Build_Bookings_View(App => App);
+
    end Update_Bookings_View;
 
 
